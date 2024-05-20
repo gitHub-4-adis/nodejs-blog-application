5f65f0c04c2002d39f51ae9b1ee2b734efb666cd
adis <temp.dev.00001@gmail.com>
2024-05-20 07:35:12 +0530

    vercel config

--- db/conn.js
+++ db/conn.js
@@ -3,10 +3,9 @@ const mongoose = require('mongoose');
 const connectMongo = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI);
-        console.log('connected with mongodb');
 
     } catch(err) {
-        console.log("Mongodb connect error: " + err);
+        if(err) throw err;
     }
 }
 
