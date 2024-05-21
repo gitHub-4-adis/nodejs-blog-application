1efe60db6abd23e265f9d7471172720eb221caa8
adis <temp.dev.00001@gmail.com>
2024-05-22 02:59:28 +0530

    router cofigured

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const router = require('../routes/router.js');
-const connectMongo = require('../db/conn.js');
+const router = require('./routes/router.js');
+const connectMongo = require('./db/conn.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended:true}));
+app.use('/', router);
+app.use(express.static(path.join(process.cwd(), 'public')));
 app.set('views', './views');
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(process.cwd(), 'public')));
 
 app.listen(PORT, ()=>{
     connectMongo();
